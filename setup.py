5692a2b0e3f50c65df4a1e779634c6353d4885a3
Phillip Schanely <pschanely+gitcmt@gmail.com>
2016-12-30 20:23:29 -0500

    v0.2; packaging updates

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ScenicOverlook',
-    version='0.1.0',
+    version='0.2.0',
     author='Phillip Schanely',
     author_email='pschanely+vE7F@gmail.com',
     packages=['scenicoverlook'],
     scripts=[],
     url='https://github.com/pschanely/ScenicOverlook',
-    license='LICENSE.txt', # (3 clause BSD)
+    license='BSD (3 clause)',
     description='A library for incremental, in-memory map-reduces',
     long_description=open('README.txt').read(),
+    keywords='algorithms datastructures',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
 )
